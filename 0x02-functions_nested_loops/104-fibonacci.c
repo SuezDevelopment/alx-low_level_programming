edf885c1e0138a80e1aab76356ba8e3a5b723117
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2023-02-21 14:17:50 -0800

    fibonacci typo III

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,21 +6,19 @@
  * Return: 0
  */
 
-int main(void)
+int main()
 {
 	int inc;
-	unsigned long n1 = 1, n2 = 2, n3;
+	unsigned long n1 = 1, n2 = 3, n3;
 
  	printf("%lu, %lu, ", n1, n2);
 
-	for (inc = 3; inc <= 98; inc++)
-	{
+	for (inc = 3; inc <= 98; inc++) {
 		n3 = n1 + n2;
 		printf("%lu, ", n3);
 		n1 = n2;
 		n2 = n3;
 	}
-
 	printf("\n");
-	return (0);
+	return 0;
 }
