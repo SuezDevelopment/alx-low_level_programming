70e5c2716f5f35d44c9a2400980f5abb47d1d7ab
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2023-02-27 02:35:16 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string followed by a new line
+ *
+ * @str: string to print the chars from
+ */
+
+
+void puts2(char *str)
+{
+	int len, x;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (x = 0; x < len; x += 2)
+	{
+		_putchar(str[x]);
+	}
+
+	_putchar('\n');
+}
