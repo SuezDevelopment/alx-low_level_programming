7832dac55d760ae912d762e0d3dea4502269285c
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2023-03-02 08:09:53 -0800

    More tasks regarding pointers, arrays, and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,20 +2,21 @@
 
 
 /**
- * reverse_array - Reverse the content of an array of integers
+ * reverse_array - reverse the content of an array of integers
  *
  * @a: the array to reverse
  * @n: the number of elements in the array
  *
- * Return: nothing
+ * Return: reversed array
  */
 
 void reverse_array(int *a, int n)
 {
-	int temp; /* holds temperary value */
+	int temp;
 	int i;
 
- 	for (i = 0; i < n/2; i++) {
+ 	for (i = 0; i < n/2; i++)
+	{
         	temp = a[i];
         	a[i] = a[n-1-i];
         	a[n-1-i] = temp;
